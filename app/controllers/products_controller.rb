348e91696d57c0339a1df9965aa714500dd4dae6
name <email>
2022-12-27 10:24:48 +0530

    search by description and name

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
 	def index
 		if params[:search].present?
-			@products = Product.where("lower(name) LIKE ?", "%#{params[:search].strip.downcase}%").paginate(page: params[:page], per_page: 3)
+			@products = Product.where("lower(name) LIKE ? OR lower(description) LIKE ?", "%#{params[:search].strip.downcase}%","%#{params[:search].strip.downcase}%").paginate(page: params[:page], per_page: 3)
 		else 
 			@products = Product.all.paginate(page: params[:page], per_page: 3)
 		end
